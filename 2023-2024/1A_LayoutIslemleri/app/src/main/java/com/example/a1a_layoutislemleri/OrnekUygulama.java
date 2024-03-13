db5de9d961ee0cfc92681f705bc1e4ac931fe410
terdemir <erdemirturgay@gmail.com>
2024-03-13 15:59:04 +0300

    layout işlemleri

---
+++ 2023-2024/1A_LayoutIslemleri/app/src/main/java/com/example/a1a_layoutislemleri/OrnekUygulama.java
@@ -0,0 +1,16 @@
+package com.example.a1a_layoutislemleri;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrnekUygulama extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ornek_uygulama);
+
+
+    }
+}
