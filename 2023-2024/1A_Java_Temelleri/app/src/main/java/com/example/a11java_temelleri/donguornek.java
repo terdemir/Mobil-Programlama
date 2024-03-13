1443c3239735808efb61898703991018fc03f543
terdemir <erdemirturgay@gmail.com>
2024-03-13 13:55:52 +0300

    ilk uygulamalar düzenlendi

---
+++ 2023-2024/1A_Java_Temelleri/app/src/main/java/com/example/a11java_temelleri/donguornek.java
@@ -0,0 +1,25 @@
+package com.example.a11java_temelleri;
+
+import java.util.Scanner;
+
+public class donguornek {
+    public static void main(String[] Args){
+        //Öğrenci sayısını kullanıcıdan alarak,
+        // tek tek geçme notu girilen bir sınıftaki öğrencilerin ortalama geçme puanını bulunuz.
+
+        Scanner deger=new Scanner(System.in);
+        int ksayisi, puan;
+        int toplam=0;
+
+        System.out.println("Öğrenci sayısını giriniz");
+        ksayisi=deger.nextInt();
+
+        for (int i=1;i<=ksayisi;i++){
+            System.out.println(i+". öğrenci notunu giriniz: ");
+            puan=deger.nextInt();
+            toplam=toplam+puan;
+        }
+        double ortalama=toplam/ksayisi;
+        System.out.println("Sınıf Ortalaması= "+ ortalama);
+    }
+}
