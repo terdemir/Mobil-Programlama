59723b0e3ac977cd56c8d5d358df2c66ffe911f1
terdemir <erdemirturgay@gmail.com>
2024-03-20 15:38:35 +0300

    1A_Hesap makinesi eklendi

---
+++ 2023-2024/1A_HesapMakinesi/app/src/main/java/com/example/hesapmakinesi/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.hesapmakinesi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText sayi1, sayi2;
+    TextView sonuc;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sayi1=findViewById(R.id.editSayi1);
+        sayi2=findViewById(R.id.editSayi2);
+        sonuc=findViewById(R.id.txtSonuc);
+
+    }
+
+    public void toplama(View view){
+        int s1=Integer.parseInt(sayi1.getText().toString());
+        int s2=Integer.parseInt(sayi2.getText().toString());
+        int toplam=s1+s2;
+        sonuc.setText("Sonuç="+toplam);
+    }
+    public void cikarma(View view){
+        int s1=Integer.parseInt(sayi1.getText().toString());
+        int s2=Integer.parseInt(sayi2.getText().toString());
+        int fark=s1-s2;
+        sonuc.setText("Sonuç="+fark);
+    }
+    public void carpma(View view){
+        int s1=Integer.parseInt(sayi1.getText().toString());
+        int s2=Integer.parseInt(sayi2.getText().toString());
+        int carpim=s1*s2;
+        sonuc.setText("Sonuç="+carpim);
+    }
+    public void bolme(View view){
+        int s1=Integer.parseInt(sayi1.getText().toString());
+        int s2=Integer.parseInt(sayi2.getText().toString());
+        double bolum=s1/s2;
+        sonuc.setText("Sonuç="+bolum);
+    }
+
+
+}
---
+++ 2023-2024/1A_LayoutIslemleri/app/src/main/java/com/example/a1a_layoutislemleri/CizgiFilmUygulama.java
@@ -0,0 +1,14 @@
+package com.example.a1a_layoutislemleri;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CizgiFilmUygulama extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cizgi_film_uygulama);
+    }
+}
